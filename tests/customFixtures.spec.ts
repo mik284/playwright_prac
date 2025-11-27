8536556b4c1742c45b18dcd5044d4077cb3fc235
mik284 <mikeykamau222@gmail.com>
2025-11-27 22:26:07 +0300

    refactor: add auto custom fixture

--- tests/customFixtures.spec.ts
+++ tests/customFixtures.spec.ts
@@ -1,6 +1,9 @@
-import { test as base, expect } from "@playwright/test";
+import { test as base, expect, Page } from "@playwright/test";
 
-const test = base.extend<{ testData: { name: string; email: string; password: string } }>({
+const test = base.extend<{
+  testData: { name: string; email: string; password: string };
+  authenticatedUser: Page;
+}>({
   testData: async ({}, use) => {
     const data = {
       name: "John Doe",
@@ -8,6 +11,20 @@ const test = base.extend<{ testData: { name: string; email: string; password: st
       password: "password123",
     };
     await use(data);
+  },
+  authenticatedUser: async ({ page, testData }, use) => {
+    await page.goto("http://binaryville.com/account");
+
+    const emailInput = page.getByRole("textbox", { name: "Email" });
+    await emailInput.fill(testData.email);
+
+    const passwordInput = page.getByRole("textbox", { name: "Password" });
+    await passwordInput.fill(testData.password);
+
+    const signInButton = page.getByRole("button", { name: "Sign in" });
+    await signInButton.click();
+
+    await use(page);
   },
 });
 
@@ -24,5 +41,13 @@ test("Should log in with  test data", async ({ page, testData }) => {
   await signInButton.click();
 
   const url = page.url();
+  expect(url).toContain(testData.password);
+});
+
+test("Should log in with  test data *using automatic custom fixture", async ({
+  authenticatedUser,
+  testData,
+}) => {
+  const url = authenticatedUser.url();
   expect(url).toContain(testData.password);
 });
