b7cddb83fcaa43d074ef857f0f11b64cba8423f9
mik284 <mikeykamau222@gmail.com>
2025-11-27 22:08:20 +0300

    update: optimized custom fixtures

---
+++ tests/customFixtures.spec.ts
@@ -0,0 +1,28 @@
+import { test as base, expect } from "@playwright/test";
+
+const test = base.extend({
+  testData: async ({}, use) => {
+    const data = {
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "password123",
+    };
+    await use(data);
+  },
+});
+
+test("Should log in with  test data", async ({ page, testData }) => {
+  await page.goto("http://binaryville.com/account");
+
+  const emailInput = page.getByRole("textbox", { name: "Email" });
+  await emailInput.fill(testData.email);
+
+  const passwordInput = page.getByRole("textbox", { name: "Password" });
+  await passwordInput.fill(testData.password);
+
+  const signInButton = page.getByRole("button", { name: "Sign in" });
+  await signInButton.click();
+
+  const url = page.url();
+  expect(url).toContain(testData.password);
+});
