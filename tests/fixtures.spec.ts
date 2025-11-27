82556f0f95777468f4ddf7541172b088768459d2
mik284 <mikeykamau222@gmail.com>
2025-11-27 21:54:25 +0300

    refactor: using built in fixtures

--- tests/fixtures.spec.ts
+++ tests/fixtures.spec.ts
@@ -1,13 +1,22 @@
-import test, { chromium, expect } from "@playwright/test"
+import test, { chromium, expect } from "@playwright/test";
 
-test('Sign In button is visible', async()=>{
-    const browser = await chromium.launch()
-    const page = await browser.newPage()
+// custom fixture
+test("Sign In button is visible", async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
 
-    await page.goto('https://binaryville.com/account')
-    const signInButton = page.getByRole('button', {name: 'Sign in'})
+  await page.goto("https://binaryville.com/account");
+  const signInButton = page.getByRole("button", { name: "Sign in" });
 
-    await expect(signInButton).toBeVisible()
+  await expect(signInButton).toBeVisible();
 
-    await browser.close()
-})
+  await browser.close();
+});
+
+// builtin fixtures
+test("Sign In button is visible *using built in fixture", async ({ page }) => {
+  await page.goto("https://binaryville.com/account");
+  const signInButton = page.getByRole("button", { name: "Sign in" });
+
+  await expect(signInButton).toBeVisible();
+});
