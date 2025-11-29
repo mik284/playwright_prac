93c621f8ba8f0203b8bfd2c4ce1a81ab135b261e
mik284 <mikeykamau222@gmail.com>
2025-11-29 09:52:05 +0300

    Extend the base test with a custom fixture to optimize test execution

---
+++ tests/POM/test.spec.ts
@@ -0,0 +1,30 @@
+import { test as base, expect } from "@playwright/test";
+import { LoginPage } from "../../page-objects/login-page.pom";
+
+
+/**
+ * Implement the Login Page form using POM, which helps separate page interactions from test logic.
+ * 1. Test Steps: The steps include importing necessary modules, creating a loginPage object, visiting the URL, filling in email and password fields, and clicking the sign-in button.
+ * 2. Test Execution: The test is run in UI mode to verify that the URL contains the expected values, confirming the test's success.
+ * 3. Benefits: Using POM makes the test easier to maintain and scale, as it separates page interactions from the test logic.
+ */
+
+// Extend the base test with a custom fixture to optimize test execution
+const test = base.extend<{ loginPage: LoginPage }>({
+    loginPage: async ({ page }, use) => {
+        const loginPage = new LoginPage(page);
+        await use(loginPage);
+    },
+});
+
+test('Should login using POM', async ({ loginPage, page }) => {
+   
+    await page.goto('http://binaryville.com/account');
+
+    await loginPage.emailLocator.fill('john.doe@example.com');
+    await loginPage.passwordLocator.fill('password123');
+
+    await loginPage.signInButtonLocator.click();
+
+    expect(page.url()).toContain('password123');
+})
