d94b34a437903b7794844a513c05e713905eb884
mik284 <mikeykamau222@gmail.com>
2025-11-29 13:40:25 +0300

    Run the browser in headless mode in CI env

--- tests/BDD/browserSetups.ts
+++ tests/BDD/browserSetups.ts
@@ -5,7 +5,16 @@ let page: Page;
 let browser: Browser;
 
 Before(async () => {
-    browser = await chromium.launch({headless: false});
+    const isCI = process.env.CI === "true";
+    browser = await chromium.launch({
+        headless: isCI ? true : false,
+        args: [
+            // "--disable-gpu",
+            "--disable-dev-shm-usage",
+            // "--no-sandbox",
+            // "--disable-features=VizDisplayCompositor"
+        ]
+    });
 
     const context = await browser.newContext();
     page = await context.newPage();
